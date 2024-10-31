b5267f6f76bc88eae8cbf5e6754120e69341f150
scholarpei <1852055776@qq.com>
2024-10-31 11:07:55 +0800

    basic_algorithm

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, jsonify
+from DS.BaseNode import NodeTag 
 
 app = Flask(__name__)
 
+# 模拟节点数据 只是个测试
+nodes = [
+    {"id": 1, "name": "Alice", "tag": NodeTag.PERSON, "relations": [2, 3]},
+    {"id": 2, "name": "Bob", "tag": NodeTag.PERSON, "relations": [1]},
+    {"id": 3, "name": "SCUT", "tag": NodeTag.UNIVERSITY, "relations": [1]},
+]
+
 @app.route('/api/nodes', methods=['GET'])
 def get_nodes():
-    # 模拟节点数据
-    nodes = [
-        {"id": 1, "name": "Alice", "tag": "PERSON", "relations": [2, 3]},
-        {"id": 2, "name": "Bob", "tag": "PERSON", "relations": [1]},
-        {"id": 3, "name": "Charlie", "tag": "GROUP", "relations": [1]},
-
-    ]
     return jsonify(nodes)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
